2d0d1fadaca7d83bffd9c0e2956e23b3e4ca2e13
davidbesd <davidbesd@gmail.com>
2023-09-27 12:27:42 +0200

    gh-pages commit

---
+++ precache-manifest.06c7e5c8b670f964c2c48c57b9da3af0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ff9babfb7cf8607f0e",
+    "url": "/pruweb/css/app.5c9713c3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pruweb/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5b40b280b6bd6379f3323a3e1bfe0f28",
+    "url": "/pruweb/index.html"
+  },
+  {
+    "revision": "ece6146076ba1c55dbbb",
+    "url": "/pruweb/js/about.544a026b.js"
+  },
+  {
+    "revision": "75ff9babfb7cf8607f0e",
+    "url": "/pruweb/js/app.5cf25b6c.js"
+  },
+  {
+    "revision": "8feafd29f87dae148f17",
+    "url": "/pruweb/js/chunk-vendors.7fb5d843.js"
+  },
+  {
+    "revision": "727f1a61ff12efcb14be56544c37d4a9",
+    "url": "/pruweb/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pruweb/robots.txt"
+  }
+]);
